7748ed4153a47bcb4399481d507ad7670c7658c1
gaurav091100 <gaurav091100@gmail.com>
2024-11-12 02:52:43 +0530

    intialized backend and made userschema and created auth page in frontend

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const databaseUrl = process.env.DATABASE_URL;
+
+
+//middleware
+
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials:true    //to enable cookie
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+const server = app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+
+//db connection
+
+mongoose
+  .connect(databaseUrl)
+  .then(() => console.log("Database connection established Successfully"))
+  .catch((err) => {
+    console.log("Something went wrong when connecting to database",err?.message);
+  });
---
+++ server/models/UserModel.js
@@ -0,0 +1,44 @@
+const { genSalt, hash } = require("bcrypt");
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: [true,"Email is required"],
+        unique:true
+    },
+    password :{
+        type: String,
+        required: [true,"Password is required"],
+    },
+    firstName :{
+        type: String,
+        required:false,
+    },
+    lastName :{
+        type: String,
+        required:false,
+    },
+    image :{
+        type: String,
+        required:false,
+    },
+    color :{
+        type: Number,
+        required:false,
+    },
+    profile : {
+        type: Boolean,
+        default: false
+    }
+})
+
+userSchema.pre("save", async function(next){
+const salt = await genSalt();
+this.password = await hash(this.password,salt);
+next();
+})
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
